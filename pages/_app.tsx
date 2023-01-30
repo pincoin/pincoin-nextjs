7e0679807e1572deb9c9e88dc0a9154365b0e5ee
pincoin <pincoins@gmail.com>
2023-01-31 00:11:56 +0900

    favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,7 @@ const App = ({Component, pageProps}: AppProps) => {
             <Head>
                 <title>핀코인 대표몰</title>
                 <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
+                <link rel="shortcut icon" href="/favicon.png" />
             </Head>
             <Component {...pageProps} />
         </>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,30 @@
 import Head from 'next/head'
+import {useEffect} from 'react';
 
 const HomePage = () => {
+    let user;
+    useEffect(() => {
+        const fetchUser = async () => {
+            const res = await fetch('http://localhost:3000/api/user')
+            const data = await res.json();
+            console.log(data);
+            return data;
+        }
+        user = fetchUser();
+    }, []);
+
+    console.log(user);
+
     return (
         <div>
             <Head>
                 <title>대한민국 1등 온라인상품권쇼핑몰 :: 핀코인</title>
-                <meta name="author" content="핀코인"/>
+                <meta name="description" itemProp="description" content="포스트 내용"/>
+                <meta name="keywords" itemProp="keywords" content="키워드,태그"/>
+                <meta property="og:site_name" content="블로그 타이틀"/>
+                <meta property="og:title" content="포스트 타이틀"/>
+                <meta property="og:url" content="토스트 주소"/>
+                <meta property="og:description" content="포스트 내용"/>
             </Head>
             <h1 className="text-3xl font-bold underline">
                 Hello world!
