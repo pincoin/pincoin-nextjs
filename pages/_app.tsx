d50a56c28f69ac751d7732283fa9400ac46647f7
pincoin <pincoins@gmail.com>
2023-01-30 22:01:27 +0900

    headless ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,7 @@ const App = ({Component, pageProps}: AppProps) => {
         <>
             <Head>
                 <title>핀코인 대표몰</title>
+                <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
             </Head>
             <Component {...pageProps} />
         </>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,16 @@
+import Head from 'next/head'
+
 const HomePage = () => {
-    return <h1 className="text-3xl font-bold underline">
-        Hello world!
-    </h1>;
+    return (
+        <div>
+            <Head>
+                <title>대한민국 1등 온라인상품권쇼핑몰 :: 핀코인</title>
+                <meta name="author" content="핀코인"/>
+            </Head>
+            <h1 className="text-3xl font-bold underline">
+                Hello world!
+            </h1>
+        </div>);
 }
 
 export default HomePage;
